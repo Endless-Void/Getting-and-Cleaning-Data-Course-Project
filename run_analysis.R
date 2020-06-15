dca79d3f1a142829c572f04fd6892250f083c871
Endless-Void <giagranadossu@unal.edu.co>
2020-06-14 19:33:48 -0500

    A lot of mistakes were fixed

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,15 @@
+library(dplyr)
 ## Get the location of every file
 GeneralFiles <- list.files("./UCI_HAR_Dataset", full.names = TRUE)
 TestFiles <- list.files("./UCI_HAR_Dataset/test", full.names = TRUE)
 TrainFiles <- list.files("./UCI_HAR_Dataset/train", full.names = TRUE)
 ## Read all relevant files from the test data files
+features <- read.table("./UCI_HAR_Dataset/features.txt", header = FALSE, col.names = c("ActivityID", "Activity"))
+
 test <- read.table("./UCI_HAR_Dataset/test/X_test.txt", header = TRUE)
-testlabels <- read.table("./UCI_HAR_Dataset/test/y_test.txt", header = TRUE)
-testsubject <- read.table("./UCI_HAR_Dataset/test/subject_test.txt", header = TRUE)
+testlabels <- read.table("./UCI_HAR_Dataset/test/y_test.txt", header = TRUE, col.names = c("Activity"))
+testsubject <- read.table("./UCI_HAR_Dataset/test/subject_test.txt", header = TRUE, col.names = c("Subject"))
+names(test) <- features$Activity
 ## Uses descriptive activity names to name the activities in the test labels data set using an "if" sintax
 for (i in 1:nrow(testlabels)) {
         if (testlabels[i,1] == 1){
@@ -24,8 +28,9 @@ for (i in 1:nrow(testlabels)) {
 }
 ## Read all relevant files from the train data files
 train <- read.table("./UCI_HAR_Dataset/train/X_train.txt", header = TRUE)
-trainlabels <- read.table("./UCI_HAR_Dataset/train/y_train.txt", header = TRUE)
-trainsubject <- read.table("./UCI_HAR_Dataset/train/subject_train.txt", header = TRUE)
+trainlabels <- read.table("./UCI_HAR_Dataset/train/y_train.txt", header = TRUE, col.names = c("Activity"))
+trainsubject <- read.table("./UCI_HAR_Dataset/train/subject_train.txt", header = TRUE, col.names = c("Subject"))
+names(train) <- features$Activity
 ## Uses descriptive activity names to name the activities in the test labels data set using an "if" sintax
 for (i in 1:nrow(trainlabels)) {
         if (trainlabels[i,1] == 1){
@@ -44,23 +49,16 @@ for (i in 1:nrow(trainlabels)) {
 }
 ##bind the subjects and labels with the test and train data
 RawTestData <- cbind(testsubject, testlabels, test)
-names(RawTestData)[1:2] <- c("Subject", "Labels")
 RawTrainData <- cbind(trainsubject, trainlabels, train)
-names(RawTrainData)[1:2] <- c("Subject", "Labels")
 ##Here I am assuming that the variables of the test and train data are the same, 
 ##which can be supported in the README file that says "The obtained dataset 
 ##has been randomly partitioned into two sets, where 70% of the volunteers
 #was selected for generating the training data and 30% the test data"
 #therefore although they have different names, they represent the same
-OriginalTestNames <- names(test)
-OriginalTrainNames <- names(train)
-names(RawTrainData) <- names(RawTestData)
 RawData<- rbind(RawTestData,RawTrainData)
-##Order All the data
-library(dplyr)
-OrderedRawData <- arrange(RawData, Subject, Labels)
-##Group all the data by subjects and Labels so we can do the mean and standard deviation in each group 
-GroupData<- group_by(OrderedRawData, Subject, Labels)
+RawDataSelected <- select(RawData, Subject, Activity, contains("mean"), contains("std"))
+##
+GroupData<- group_by(RawDataSelected, Subject, Activity)
 ResultData <- summarize_all(GroupData, funs(mean, sd))
-ResultData
-data <- write.table(ResultData,file = "tidydata.txt")
+tbl_df(ResultData)
+##data <- write.table(ResultData,file = "tidydata.txt")
